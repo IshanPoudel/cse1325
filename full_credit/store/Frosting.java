f0f65cf719f31bb2e541997ba71b3f6c5cf2711f
IshanPoudel <ishan.poudel@mavs.uta.edu>
2021-10-19 00:37:58 -0500

    Delete full_credit directory

--- full_credit/store/Frosting.java
+++
@@ -1,5 +0,0 @@
-public enum Frosting
- 	{
-
-	unfrosted , glazed , chocolate , vanilla , strawberry , creme , cherry
-    }
--- full_credit/store/Shot.java
+++
@@ -1,3 +0,0 @@
-public enum Shot{
-		none , milk , sugar , creamer ,  chocolate , vanilla , peppermint , hazlenut
-	}
